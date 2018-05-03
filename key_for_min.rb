de5e6c671017f57264313c0dbae3e7f24779128e
Precious Humphrey <phumphrey23@gmail.com>
2018-05-03 04:20:53 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,7 +10,7 @@ def key_for_min_value(name_hash)
     #min_key = name_hash.first[1]
     #min_value = name_hash.first[0]
     name_hash.each do |key, value|
-      if min_value > value
+      if min_value == nil || value < min_value
         min_value = value
         min_key = key
     end
